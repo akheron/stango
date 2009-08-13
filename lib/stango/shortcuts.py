0c91b7546dcf770c5c1f90bb41ad2de1998a62bb
Petri Lehtinen <petri@digip.org>
2009-08-13 21:47:31 +0300

    Use global Jinja2 environment in render_template
    Before this patch, a new environment was created in every call of
    render_template().

--- lib/stango/shortcuts.py
+++ lib/stango/shortcuts.py
@@ -1,9 +1,12 @@
 import os
 
+_env = None
 def render_template(template_name, **kwargs):
     from jinja2 import Environment, FileSystemLoader
+    global _env
 
-    env = Environment(loader=FileSystemLoader('templates'))
-    tmpl = env.get_template(template_name)
+    if _env is None:
+        _env = Environment(loader=FileSystemLoader('templates'))
 
+    tmpl = _env.get_template(template_name)
     return tmpl.render(**kwargs)
