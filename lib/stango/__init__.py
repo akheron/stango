a18f8bd7d301abe0f98f86218e70bddc3039c683
Petri Lehtinen <petri@digip.org>
2009-09-04 23:57:34 +0300

    Support viewing contents of tarballs

--- lib/stango/__init__.py
+++ lib/stango/__init__.py
@@ -1,4 +1,6 @@
+import operator
 import os
+import tarfile
 
 class File(object):
     def __init__(self, path, view, kwargs):
@@ -34,3 +36,28 @@ def files(*args):
             yield File(path, view, kwargs)
 
     return list(_files(*args))
+
+
+def view_file_from_tar(tar, member):
+    return tar.extractfile(member).read()
+
+
+def files_from_tar(basepath, tarname, strip=0):
+    tar = tarfile.open(tarname, 'r')
+    def _files():
+        for member in tar.getmembers():
+            if not member.isfile():
+                continue
+            if strip > 0:
+                parts = member.name.split('/')[strip:]
+                if not parts:
+                    continue
+                served_name = os.path.join(*parts)
+            else:
+                served_name = member.name
+            filename = os.path.join(basepath, served_name)
+            yield files((
+                    filename,
+                    view_file_from_tar,
+                    { 'tar': tar, 'member': member.name }))
+    return reduce(operator.add, _files())
