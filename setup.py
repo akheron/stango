0e578e185dfcd19ac4d2d26679f06d9aee744213
Petri Lehtinen <petri@digip.org>
2010-08-20 21:35:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@
 from distutils.core import setup
 
 setup(
-    name='stango',
+    name='Stango',
     version='0.1',
     author='Petri Lehtinen',
     author_email='petri@digip.org',
     url='http://www.digip.org/stango/',
-    description='Static web site generator for Python programmers',
+    description='Pythonic static web site generator',
     long_description='''Stango is a Pythonic static web site generator
 that feels like a web framework. User defined view functions generate
 the content to pages (e.g. using Jinja2 templates or any other means).
 During development, the pages are served locally using a built-in web
-server with all URLs working. When they site is ready, it is generated
+server with all URLs working. When the site is ready, it is generated
 to flat files and can be pushed to a production server.''',
     license='MIT',
-    packages=['stango', 'stango.manage'],
+    packages=['stango'],
     scripts=['bin/stango'],
 )
