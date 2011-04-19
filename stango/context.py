995c77f8c360e60d720ea98f6b50e77ee25298ba
Petri Lehtinen <petri@digip.org>
2011-04-19 20:55:58 +0300

    Create the jinja2 envionment already in Context's __init__
    This makes it available earlier.

--- stango/context.py
+++ stango/context.py
@@ -10,7 +10,16 @@ class Context(object):
         self.mode = mode
         self.path = filespec.path
         self.realpath = filespec.realpath(manager.index_file)
-        self.jinja_env = None
+
+        try:
+            self.jinja_env = self.manager._jinja_env
+        except AttributeError:
+            # Create the Jinja environment once and save it to the
+            # manager instance
+            from jinja2 import Environment, FileSystemLoader
+            loader = FileSystemLoader(self.manager.template_dirs)
+            env = Environment(loader=loader)
+            self.jinja_env = self.manager._jinja_env = env
 
     def render_template(self, template_name, **kwargs):
         builtin_template_args = {
@@ -19,17 +28,6 @@ class Context(object):
             'path': self.path,
             'realpath': self.realpath,
         }
-
-        if not self.jinja_env:
-            try:
-                self.jinja_env = self.manager._jinja_env
-            except AttributeError:
-                # Create the Jinja environment once and save it to the
-                # manager instance
-                from jinja2 import Environment, FileSystemLoader
-                loader = FileSystemLoader(self.manager.template_dirs)
-                env = Environment(loader=loader)
-                self.jinja_env = self.manager._jinja_env = env
 
         template = self.jinja_env.get_template(template_name)
         return template.render(dict_merge(builtin_template_args, kwargs))
